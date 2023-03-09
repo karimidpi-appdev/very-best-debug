0f80e8dd0930e4bdd33beaa28cf4546e0e10582e
Mohammad Ali Karimi <ali216k@gmail.com>
2023-03-09 05:07:15 +0000

    Update venue works!

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -26,11 +26,11 @@ class VenuesController < ApplicationController
   end
   
   def update
-    the_id = params.fetch("venue_id")
+    the_id = params.fetch("the_id")
 
-    @venue = Venue.where({ :id => the_id })
+    venue = Venue.where({ :id => the_id }).first
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
+    venue.name = params.fetch("query_name")
     venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
     
